dfe2c388af7b0820f4d63fff34e9f14405bdf632
Jianing Wang <xi.jianing.xixi@gmail.com>
2025-01-11 01:22:10 +1100

    add size

--- include/Ir/IrType.h
+++ include/Ir/IrType.h
@@ -196,6 +196,10 @@ public:
         return dimension;
     }
 
+    int getDimensionSize() const {
+        return dimension.size();
+    }
+
     IrTypeArray* clone() const override {
         return new IrTypeArray(*this);
     }
