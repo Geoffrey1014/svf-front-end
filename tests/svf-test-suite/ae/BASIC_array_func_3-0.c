ccd3673803067558fed367f2f16cfab7f54f4340
Jianing Wang <xi.jianing.xixi@gmail.com>
2025-01-13 00:07:35 +1100

    change the dependency, typedef as the type

--- tests/svf-test-suite/ae/BASIC_array_func_3-0.c
+++ tests/svf-test-suite/ae/BASIC_array_func_3-0.c
@@ -2,26 +2,26 @@
 // Created by Jiawei Wang on 2/3/22.
 //
 
-#include "stdbool.h"
-extern void svf_assert(bool);
+// #include "stdbool.h"
+// extern void svf_assert(bool);
 
 typedef struct {
     int a;
     int b[2];
 } A;
 
-int getValue(A* arr, int x) {
-    return arr->b[x];
-}
+// int getValue(A* arr, int x) {
+//     return arr->b[x];
+// }
 
-int main() {
-    A a;
-    a.a = 0;
-    a.b[0] = 1;
-    a.b[1] = 2;
-    svf_assert(getValue(&a, 1) == 2);
-    return 0;
-}
+// int main() {
+//     A a;
+//     a.a = 0;
+//     a.b[0] = 1;
+//     a.b[1] = 2;
+//     svf_assert(getValue(&a, 1) == 2);
+//     return 0;
+// }
 
 // Oirignal code but has the ERROR (tree-sitter-c)
 // struct {
