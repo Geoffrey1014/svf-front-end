52484e152c68316e482d5a802287e15483404209
geo <geoffreyhe2@gmail.com>
2025-01-23 19:48:31 +1100

    refactor: move popFromStack implementation from ASTBuilder.tpp to ASTBuilder.h; remove ASTBuilder.tpp

--- include/ASTBuilder.h
+++ include/ASTBuilder.h
@@ -96,5 +96,26 @@ public:
     }
 };
 
-#include "ASTBuilder.tpp"
+///////// Catch Print Template ////////
+template <typename T>
+T* ASTBuilder::popFromStack(const TSNode& node) {
+    if (this->ast_stack.empty()) {
+        throw std::runtime_error("Error: AST stack is empty while attempting to pop node:\n" + getNodeText(node));
+    }
+
+    Ir* top = this->ast_stack.top();
+    this->ast_stack.pop();
+
+    if (T* casted = dynamic_cast<T*>(top)) {
+        return casted;
+    } else {
+        std::string errorMessage = "Error: Invalid type on AST stack.\n";
+        errorMessage += "Expected type: " + std::string(typeid(T).name()) + "\n";
+        errorMessage += "Actual type: " + std::string(typeid(*top).name()) + "\n";
+        errorMessage += "Node content: " + getNodeText(node) + "\n";
+        delete top; // Clean up the invalid node
+        throw std::runtime_error(errorMessage);
+    }
+}
+
 #endif
